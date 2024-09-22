a701a8ce7c6197645b0ce685bf6218dbc11e168c
Dominik Chovanec <dominkchov@gmail.com>
2024-09-22 22:12:26 +0200

    addind nav

--- web.js
+++ web.js
@@ -3,11 +3,17 @@ import cors from 'cors';
 import 'dotenv/config';
 import bodyParser from 'body-parser';
 import {getRoutes} from "./routes/get-routes.js";
+import  { fileURLToPath } from 'url';
+import { dirname, resolve} from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/images', express.static(resolve(__dirname, './images')));
 app.set('view engine', 'ejs');
 getRoutes(app);
 
